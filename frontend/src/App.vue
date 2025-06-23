aefa4d4d3a8cc05738d24ec7ff2f766719818af9
yanldx <114926335+yanldx@users.noreply.github.com>
2025-06-23 22:44:06 +0200

    feat(frontend): use UserManagement component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import UserForm from './components/UserForm.vue'
-import UserList from './components/UserList.vue'
-
-const listRef = ref()
-function refreshList() {
-  if (listRef.value) listRef.value.loadUsers()
-}
+import UserManagement from './components/UserManagement.vue'
 </script>
 
 <template>
-  <h1>User Management</h1>
-  <UserForm @created="refreshList" />
-  <UserList ref="listRef" />
+  <UserManagement />
 </template>
-
-<style scoped>
-h1 {
-  margin-bottom: 1rem;
-}
-</style>
