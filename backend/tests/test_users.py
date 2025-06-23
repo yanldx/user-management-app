f88800688ec6ac5792e7d4be791ca79ce5ed8868
yanldx <yan.ledoux@ynov.com>
2025-06-23 12:38:05 +0200

    tests

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -1,4 +1,8 @@
-import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from fastapi.testclient import TestClient
 from app.main import app
 
