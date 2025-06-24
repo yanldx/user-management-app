d1e7007365b3233e8334d11e21d7dd9efca9f6e0
yanldx <yan.ledoux@ynov.com>
2025-06-24 16:06:49 +0200

    deploy CI/CD

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -1,9 +1,10 @@
 import sys
 import os
-from fastapi.testclient import TestClient
-from app.main import app
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from fastapi.testclient import TestClient
+from app.main import app
 
 client = TestClient(app)
 
