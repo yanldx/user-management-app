094810efa0fa46fca16969ea2f3c0f10dfd63627
yanldx <yan.ledoux@ynov.com>
2025-06-24 10:18:17 +0200

    fix: corrected imports for Vercel deployment

--- backend/api/index.py
+++ backend/api/index.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
-from .init_admin import create_admin_from_env
-from .routers.users import router as users_router
+from app.init_admin import create_admin_from_env
+from app.routers.users import router as users_router
 
 app = FastAPI()
 
